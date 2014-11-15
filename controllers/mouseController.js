5ce6160f6d7bc4d6021a83a7043d25a8c943a342
Nik <nik.pursiainen@hotmail.com>
2014-11-15 14:40:41 -0500

    Updated to remove array use

--- controllers/mouseController.js
+++ controllers/mouseController.js
@@ -1,14 +1,11 @@
 (function() {
     
     var paint, context;
-    var clickX = new Array();
-    var clickY = new Array();
-    var clickDrag = new Array();
+    var clickX, clickY;
 
     context = document.getElementById('whiteBoard').getContext("2d");
 
     document.onmousemove = handleMouseMove;
-    document.onclick = handleMouseClick;
     document.onmousedown = handleMouseDown;
     document.onmouseup = handleMouseUp;
 
@@ -17,12 +14,8 @@
         console.log(isNaN(event.pageX) + "   " + $('whiteBoard').left);
         if(paint) {
             addClick(event.pageX, event.pageY, true);
-            redraw();
+            //redraw();
         }
-    }
-
-    function handleMouseClick(event) {
-        console.log("MOUSE CLICK");    
     }
 
     function handleMouseDown(event) {
@@ -35,31 +28,8 @@
         paint = false;
     }
 
-    function addClick(x, y, dragging) {
-        clickX.push(x);
-        clickY.push(y);
-        clickDrag.push(dragging);
-        console.log("Adding point: (" + x + ", " + y + ") Draw: " + dragging);
-    }
-
-    function redraw() {
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-
-        context.strokeStyle = "#df4b26";
-        context.lineJoin = "round";
-        context.lineWidth = 5;
-
-        for(var i=0; i < clickX.length; i++) {
-            context.beginPath();
-            if(clickDrag[i] && i) {
-                context.moveTo(clickX[i-1], clickY[i-1]);
-            }else{
-                context.moveTo(clickX[i-1], clickY[i]);
-            }
-            context.lineTo(clickX[i], clickY[i]);
-            context.closePath();
-            context.stroke();
-        }
+    function addClick(x, y) {
+        context.fillRect(x+1, y+1, 2, 2);
     }
 
 })();
