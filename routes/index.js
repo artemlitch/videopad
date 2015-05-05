ff7aee8c64b05d9573dcc24ee4976ec1031d5536
Tom <tom2zip@gmail.com>
2015-05-05 11:21:10 -0400

    Changed the title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   //res.render('index', { title: 'Express' });
-  res.render('main', { title: 'Whiteboard' });
+  res.render('main', { title: 'Brainboard' });
 });
 
 module.exports = router;
