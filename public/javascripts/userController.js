15e5256e2df60135578242b99a61dd711e98b1f9
Mash <mike_ashcroft09@hotmail.com>
2015-07-10 20:53:53 -0400

    Hidden Room Keys
    Have to click in order to see room keys

--- public/javascripts/userController.js
+++ public/javascripts/userController.js
@@ -3,16 +3,14 @@ $(document).ready(function() {
     var context = canvas.getContext('2d');
     var socket = io();
     socket.on('userInfo' , function(user) {
-       // console.log("welcome "+ user.id);
+       console.log("welcome "+ user.id);
     });
     socket.on('roomCreateConf' , function(user) {
-       $('#roomNameLabel').text(user.room);
+       $('#roomNameLabel').text("Click To Show Room Key");
     });
     socket.on('roomJoinConf' , function(user) {
-       $('#roomNameLabel').text(user.room);
-       var image = new Image();
-      // image.src = user.roomCanvas;
-      // context.drawImage(image,0,0);
+       $('#roomNameLabel').text("Click To Show Room Key");
+
     });
 	 
         
